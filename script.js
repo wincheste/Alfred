c0d730c6fc941f080d8d4b34bf9a65677d9da462
bananaWeb <a89.volkov@gmail.com>
2018-02-08 21:11:35 +0300

    array+object

--- script.js
+++ script.js
@@ -1,20 +1,138 @@
 $(document).ready(function() {
 
-	$('.js-sayThat').click(function(){
-		showbubble($('.js-text1').val() + $('.js-text2').val())
+
+
+		let superhero = {
+			name:' Kiruha',
+			age:32,
+			job:'artist',
+
+			params:{
+				height:130,
+				weight:45,
+				gender:'Male',
+				race:'european'
+			},
+
+			powers:{
+				mainPower:'brush punch',
+				secondaryPower:'sleep 8 hours'
+			}
+		};
+
+
+			$('.js-object').click(function(){
+				showBubbleWithBlock('<div class="bubble-test">Меня зовут- '+ superhero.name + '! </div>' );
+			});
+
+
+
+
+
+
+			let movies = ['apple', 'orange', 'banana','door'];
+
+
+			$('.js-array').click(function(){
+				movies.push(getSecondWord());
+				showBubble(movies);
+			})
+
+
+			for (let i=0; i<movies.length; i++){
+				$('.test-array').append('<div class="test-item">' + movies[i] +'</div>')
+			};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	$('.js-plus').click(function() {
+		showBubble(Number(getFirstWord()) + Number(getSecondWord()));
 	});
 
-	function showbubble(text) {
+	$('.js-minus').click(function() {
+		showBubble(Number(getFirstWord()) - Number(getSecondWord()));
+	});
+
+	$('.js-multiply').click(function() {
+		showBubble(Number(getFirstWord()) * Number(getSecondWord()));
+	});
+
+	$('.js-divide').click(function() {
+		if (checkInputData().isNumber) {
+			if (getSecondWord()==="0") {
+				showBubble('так вымерли динозавры')
+			}
+			showBubble(Number(getFirstWord()) / Number(getSecondWord()));
+		} else {
+			if (checkInputData().isEmpty) {
+				showBubble('Пустоши какие-то!');
+			} else {
+				showBubble(getFirstWord() + ' ' + getSecondWord());
+			}
+		}
+	});
+
+	// ===============================
+	// ===============================
+
+	function checkInputData() {
+		if ($.isNumeric(getFirstWord()) && $.isNumeric(getSecondWord())) {
+			return {
+				isNumber: true
+			};
+
+		} else if (getFirstWord() === '' && getSecondWord() === '') {
+			return {
+				isNumber: false,
+				isEmpty: true
+			};
+
+		} else {
+			return {
+				isNumber: false,
+				isEmpty: false
+			};
+		}
+	}
+
+	function getFirstWord() {
+		return $('.js-text1').val();
+	}
+
+	function getSecondWord() {
+		return $('.js-text2').val();
+	}
+
+	function showBubble(text) {
 		$('.bubble').text(text);
 		$('.bubble').addClass('-visible');
 
 
 		setTimeout(function() {
- 			$('.bubble').removeClass('-visible');
+			$('.bubble').removeClass('-visible');
 		}, 2000);
+
 	}
 
+	function showBubbleWithBlock(block) {
+		$('.bubble').append(block);
+		$('.bubble').addClass('-visible');
 
+
+		setTimeout(function() {
+			$('.bubble').removeClass('-visible');
+		}, 2000);
+
+	}
 });
-
-
